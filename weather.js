0b3b1788c2f1a3bb62dab88d03c5e180a7d62f00
Halimatou <halimatou.saddiyaa@gmail.com>
2025-07-23 12:35:16 +0100

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,18 @@
+const mockWeatherData = {
+  Paris: {
+    temp: 23,
+    description: "Partly cloudy",
+  },
+  London: {
+    temp: 18,
+    description: "Light rain",
+  },
+  Madrid: {
+    temp: 30,
+    description: "Sunny",
+  },
+  Default: {
+    temp: 25,
+    description: "Clear sky",
+  }
+};
